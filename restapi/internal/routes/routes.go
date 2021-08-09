47726c2718abbe429252b510fe553b215b5d6323
Lachezar Bogomilov <lachezar.bogomilov@sap.com>
2021-08-09 11:28:04 +0300

    add readiness check

--- restapi/internal/routes/routes.go
+++ restapi/internal/routes/routes.go
@@ -1,10 +1,11 @@
 package routes
 
 const (
-	AllCryptosURL   = "/api/cryptos"
-	SingleCryptoURL = "/api/cryptos/{crypto_id}"
-	AddCryptoURL    = "/api/cryptos"
-	UpdateCryptoURL = "/api/cryptos/{crypto_id}"
-	RemoveCryptoURL = "/api/cryptos/{crypto_id}"
-	HealthCheckURL  = "/api/health"
+	AllCryptosURL     = "/api/cryptos"
+	SingleCryptoURL   = "/api/cryptos/{crypto_id}"
+	AddCryptoURL      = "/api/cryptos"
+	UpdateCryptoURL   = "/api/cryptos/{crypto_id}"
+	RemoveCryptoURL   = "/api/cryptos/{crypto_id}"
+	HealthCheckURL    = "/api/health"
+	ReadinessCheckURL = "/api/ready"
 )
